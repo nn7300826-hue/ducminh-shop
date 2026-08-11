21c8e25499ba3da9e46f6c9c923dc3fa2e2d9be5
nn7300826-hue <nn7300826@gmail.com>
2026-08-11 09:40:59 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,7 @@
 const SUPABASE_URL = "https://hiijgyfyntscoutnhmkv.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_0Rywaae4_7YkE29h45RgGQ_XttGtTWj";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
